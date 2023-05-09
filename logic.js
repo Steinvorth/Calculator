2a7d9effae404130d5e8097ad3ce9a935d015cca
Emiliano <emilianosteinvorth@gmail.com>
2023-05-09 16:35:39 -0600

    Working input for numbers one and two

--- logic.js
+++ logic.js
@@ -1,20 +1,30 @@
 let inputNum1 = '';
 let inputNum2 = '';
+let operator = '';
+const previousNum = document.getElementById("prevNum");
 const answer = document.getElementById("answer");
 const clearAll = document.getElementById('clear');
 
 clearAll.addEventListener('click', ()=>{
     answer.value = '';
+    previousNum.value = '';
+    inputNum1 = '';
+    inputNum2 = '';
+    operator = '';
 });
 
 function getNumber(num){
-    if(inputNum1 === ''){
+    if(operator === ''){
         inputNum1 += num;
+        previousNum.value = inputNum1;
         answer.value = inputNum1;
+        console.log("num 1:" + inputNum1)
     }
     else{
         inputNum2 += num;
+        previousNum.value = inputNum1 + '' + operator + '' + inputNum2;
         answer.value = inputNum2;
+        console.log("num2: " + inputNum2)
     }
     
 }
@@ -23,6 +33,7 @@ function add(a, b){
     return a + b;
 }
 
-function calculate(){
-
+function setOperator(op){
+    operator = op;
+    previousNum.value = inputNum1 + '' +operator;
 }
