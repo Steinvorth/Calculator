5f4abf1fc902c3a794b6ab85717158e2180de9f3
Emiliano <emilianosteinvorth@gmail.com>
2023-05-10 19:04:41 -0600

    All buttons work as expected, logic.js is working.

--- logic.js
+++ logic.js
@@ -1,12 +1,28 @@
 let inputNum1 = '';
 let inputNum2 = '';
 let operator = '';
+let decAdded = false;
+let negative = false;
 const previousNum = document.getElementById("prevNum");
 const answer = document.getElementById("answer");
 const clearAll = document.getElementById('clear');
 const equals = document.getElementById("equals");
 const decimal = document.getElementById("decimal");
 const lastDel = document.getElementById("delete");
+const negate = document.getElementById("negate");
+
+negate.addEventListener('click', () =>{
+    if(operator === ''){
+        inputNum1 = toggleSign(inputNum1);
+        previousNum.value = inputNum1;
+        answer.value = inputNum1;
+    }
+    else{
+        inputNum2 = toggleSign(inputNum2);
+        previousNum.value = inputNum1 + '' + operator + '' + inputNum2;
+        answer.value = inputNum2;
+    }
+});
 
 //clear all button
 clearAll.addEventListener('click', ()=>{
@@ -15,6 +31,7 @@ clearAll.addEventListener('click', ()=>{
     inputNum1 = '';
     inputNum2 = '';
     operator = '';
+    
 });
 
 //equal button
@@ -22,40 +39,74 @@ equals.addEventListener('click', ()=>{
     calc();
 })
 
+ //Deletes last character Added to the number
+lastDel.addEventListener('click', ()=>{
+    if(operator === ''){
+        inputNum1 = del(inputNum1);
+        previousNum.value = inputNum1;
+        answer.value = inputNum1;
+    }
+    else if(inputNum2 !== ''){
+        inputNum2 = del(inputNum2);
+        previousNum.value = inputNum1 + '' + operator + '' + inputNum2;
+        answer.value = inputNum2;
+    }
+    else{
+        operator = '';
+        previousNum.value = inputNum1;
+    }
+    
+});
+
+//Checks if there is a decimal on the inputNumber
+function getDecimal(numIn){
+    if(numIn.includes('.')){
+        decimal.disabled = true;
+    }
+    else{
+        decimal.disabled = false;
+    }
+}
+
+//Delete Las Number Function
 function del(num){
     if(num !== ''){
         num = num.slice(0,-1);
         console.log("new number1:" + num)
-        previousNum.value = num;
-        answer.value = num;
+    }
+    return num;
+}
+
+//Toggles negative sign on the number
+function toggleSign(num){
+    if(negative){
+        negative = false;
+        return num.substring(1);
+    }
+    else{
+        negative = true;
+        return '-' + num;
     }
 }
 
-//Gets button input
+//Gets Number input
 function getNumber(num){
     if(operator === ''){
-        
-        decimal.addEventListener('click', ()=>{
-            decimal.disabled = true;
-        });
-
-        //Deletes last character Added to the number
-        lastDel.addEventListener('click', ()=>{
-            del(inputNum1);
-        });
-
+        // decimal.addEventListener('click', ()=>{
+        //     decimal.disabled = true;
+        // });
         inputNum1 += num;
         previousNum.value = inputNum1;
         answer.value = inputNum1;
-        console.log("num 1:" + inputNum1)
-
-        
+        console.log("num 1:" + inputNum1);   
+        getDecimal(inputNum1);     
     }
-    else{
+    else{        
         inputNum2 += num;
         previousNum.value = inputNum1 + '' + operator + '' + inputNum2;
         answer.value = inputNum2;
         console.log("num2: " + inputNum2)
+        getDecimal(inputNum2);
     }
 }
 
@@ -92,10 +143,16 @@ function modulus(a ,b){
 
 //Takes operator as input for function calc later 
 function setOperator(op){
-    operator = op;
-    previousNum.value = inputNum1 + '' +operator;
+    if(inputNum1 && inputNum2){
+        calc();
+        inputNum1 = answer.value;
+        inputNum2 = '';
+    }
 
     decimal.disabled = false;
+
+    operator = op;
+    previousNum.value = inputNum1 + '' +operator;
 
     if(operator === "="){
         inputNum2 = '';
@@ -124,7 +181,8 @@ function calc (){
         default:
             result = 0;
     }
-    inputNum1 = result.toString();
+    answer.value = result.toString();
+    inputNum1 = '';
     inputNum2 = '';
     operator = '';
     decimal.disabled = false;
